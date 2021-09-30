1f4dfc98ff38fad7d9a61d71aa0dad21ce45d23f
Donald Vida <donald.vida@gmail.com>
2021-09-30 16:48:52 -0500

    Implemented getMetaData queries, and parsed combined data into a response.

--- db/reviewsMeta.js
+++ db/reviewsMeta.js
@@ -9,16 +9,30 @@ const getMetaData = (productId, callback) => {
     .then((res) => {
       metaData.ratings = {};
 
-      res.rows.forEach((rating) => {
-        metaData.ratings[rating.rating] = rating.count;
+      res.rows.forEach((rat) => {
+        metaData.ratings[rat.rating] = rat.count;
       });
 
       return psql.query(`SELECT recommend as recommended, COUNT (recommend) FROM reviews WHERE product_id = ${productId} GROUP BY recommend`);
     }).then((res) => {
       console.log('recommended:', res.rows);
+      metaData.recommended = {};
+
+      res.rows.forEach((rec) => {
+        metaData.recommended[rec.recommended] = rec.count;
+      });
+
       return psql.query(`SELECT ch.name as name, ch.characteristic_id as id, AVG(chr.value) as value FROM characteristics as ch INNER JOIN characteristic_reviews as chr ON ch.characteristic_id = chr.characteristic_id WHERE product_id = ${productId} GROUP BY ch.characteristic_id`);
     }).then((res) => {
       console.log('characteristics:', res.rows);
+      metaData.characteristics = {};
+
+      res.rows.forEach((char) => {
+        metaData.characteristics[char.name] = {
+          id: char.id,
+          value: char.value,
+        };
+      });
       callback(null, metaData);
     })
     .catch((e) => callback(e));
@@ -27,10 +41,10 @@ const getMetaData = (productId, callback) => {
 module.exports = getMetaData;
 
 // TESTING
-getMetaData(1, (e, metaData) => {
-  if (e) {
-    console.error(e.stack);
-  } else {
-    console.log(JSON.stringify(metaData, null, '  '));
-  }
-});
+// getMetaData(1, (e, metaData) => {
+//   if (e) {
+//     console.error(e.stack);
+//   } else {
+//     console.log(JSON.stringify(metaData, null, '  '));
+//   }
+// });
