e6e09b2b179ec5fc842f155c786b4097794786a6
Donald Vida <donald.vida@gmail.com>
2021-09-30 12:41:08 -0500

    Implemented routes for HTTP server

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 const port = 3030;
 
 /**
@@ -16,12 +17,46 @@ const port = 3030;
 // /reviews
 
 app.get('/reviews', (req, res) => {
-  if (!req.query?.product_id) {
-    res.send(`Successfully called reviews for ${req.query.product_id}!`);
+  console.log('GET reviews request:', JSON.stringify(req.query));
+  if (req.query?.product_id) {
+    res.send(`Successfully called reviews for product ${req.query.product_id}!`);
   } else {
-    res.status(400).send('Please provide a product_id');
+    res.status(400).send('Please provide a product_id parameter');
   }
 });
+
+app.post('/reviews', (req, res) => {
+  console.log('POST reviews request:', JSON.stringify(req.body));
+  if (req.body?.product_id) {
+    res.send(`Successfully created a review for product ${req.body.product_id}!`);
+  } else {
+    res.status(400).send('Please include a product_id in the body of your request.');
+  }
+});
+
+// reviews/meta
+
+app.get('/reviews/meta', (req, res) => {
+  console.log('GET reviews/meta request:', JSON.stringify(req.query));
+  if (req.query?.product_id) {
+    res.send(`Succcessfully called reviews meta for product ${req.query.product_id}!`);
+  } else {
+    res.status(400).send('Please provide a product_id parameter');
+  }
+});
+
+// reviews/:review_id flagging
+app.put('/reviews/:review_id/helpful', (req, res) => {
+  console.log(`PUT reviews/${req.params.review_id}/helpful request received`);
+  res.send(`Successfully marked review ${req.params.review_id} as helpful.`);
+});
+
+app.put('/reviews/:review_id/report', (req, res) => {
+  console.log(`PUT reviews/${req.params.review_id}/report request received`);
+  res.send(`Successfully marked review ${req.params.review_id} as reported.`);
+});
+
+// Listening
 
 app.listen(port, (err) => {
   if (err) {
