fcb7c7f0184f345b3bad677016b2185462f2afb0
Donald Vida <donald.vida@gmail.com>
2021-09-30 15:19:26 -0500

    Implemented Connector.js file to handle postgres operations

---
+++ db/connector.js
@@ -0,0 +1,10 @@
+const { pgConfig } = require('../config.js')
+const { Pool } = require('pg');
+const psql = new Pool(pgConfig);
+
+// TESTING
+// psql.query('SELECT * FROM reviews LIMIT 5')
+//   .then(res => console.log('Successful query:', res.rows))
+//   .catch(e => console.error(e.stack));
+
+module.exports = psql
