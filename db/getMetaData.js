e47e1a5be994e0eb5e28cdef5a6130b69287dd8d
Donald Vida <donald.vida@gmail.com>
2021-10-01 11:41:56 -0500

    Implemented HTTP request and psql query for /reviews endpoint

--- db/getMetaData.js
+++ db/getMetaData.js
@@ -21,7 +21,7 @@ const getMetaData = (productId, callback) => {
         metaData.recommended[rec.recommended] = rec.count;
       });
 
-      return psql.query(`SELECT ch.name as name, ch.characteristic_id as id, AVG(chr.value) as value FROM characteristics as ch INNER JOIN characteristic_reviews as chr ON ch.characteristic_id = chr.characteristic_id WHERE product_id = ${productId} GROUP BY ch.characteristic_id`);
+      return psql.query(`SELECT ch.name as name, ch.characteristic_id as id, AVG(chr.value) as value FROM characteristics as ch INNER JOIN characteristic_reviews as chr ON ch.characteristic_id = chr.characteristic_id WHERE ch.product_id = ${productId} GROUP BY ch.characteristic_id`);
     }).then((res) => {
       metaData.characteristics = {};
 
---
+++ db/getReviewsData.js
@@ -0,0 +1,63 @@
+const psql = require('./connector.js');
+
+const getReviewsData = ({ product_id, sort = 'newest', count = 5, page = 0 }, callback) => {
+  const offset = count * page;
+  const reviewsData = {
+    product_id,
+    page,
+    count,
+  };
+
+  const selectReviews = `SELECT review_id, rating, summary, recommend, response, body, date, reviewer_name, helpfulness FROM reviews  WHERE product_id = ${product_id} `;
+  const fetchReviews = ` OFFSET ${offset} ROWS FETCH FIRST ${count} ROW ONLY `;
+  let sortReviews = 'ORDER BY date DESC'; // will implement relevance
+
+  switch (sort) {
+    case 'helpful':
+      sortReviews = 'ORDER BY helpfulness DESC';
+      break;
+    case 'newest':
+      sortReviews = 'ORDER BY date DESC';
+      break;
+    default:
+      break;
+  }
+
+  psql.query(selectReviews + sortReviews + fetchReviews)
+    .then((res) => {
+      if (res.rows.length) {
+        const reviewsObject = {};
+        const reviewIDList = [];
+        res.rows.forEach((row) => {
+          reviewsObject[row.review_id] = { ...row, photos: [] };
+          reviewIDList.push(row.review_id);
+        });
+        psql.query(`SELECT * FROM photos WHERE review_id IN (${reviewIDList.join(', ')})`)
+          .then((photoRes) => {
+            if (photoRes.rows.length) {
+              photoRes.rows.forEach((row) => {
+                reviewsObject[row.review_id].photos.push(row);
+              });
+            }
+            reviewsData.results = Object.values(reviewsObject);
+            callback(null, reviewsData);
+          })
+          .catch((e) => callback(e));
+      } else {
+        reviewsData.results = [];
+        callback(null, reviewsData);
+      }
+    })
+    .catch((e) => callback(e));
+};
+
+module.exports = getReviewsData;
+
+// TESTING
+// getReviewsData({ product_id: 2 }, (err, reviewsData) => {
+//   if (err) {
+//     console.log('reviews query failed', err.stack)
+//   } else {
+//     console.log(JSON.stringify(reviewsData, null, '  '));
+//   }
+// });
