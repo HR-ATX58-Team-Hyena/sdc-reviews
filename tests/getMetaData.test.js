ffc6ac4df34fbe8d5423c152afdd1b1768e969b4
Donald Vida <donald.vida@gmail.com>
2021-10-02 12:59:11 -0500

    Additional cleanup

--- tests/getMetaData.test.js
+++ tests/getMetaData.test.js
@@ -2,10 +2,10 @@ const { getMetaData } = require('../db/modules.js');
 
 test('getMetaData should pass back an object, but not an array', done => {
   let callback = (err, data) => {
-      expect(typeof data).toBe('object');
-      expect(Array.isArray(data)).toBe(false);
-      expect(err).toBe(null)
-      done();
+    expect(err).toBe(null)
+    expect(typeof data).toBe('object');
+    expect(Array.isArray(data)).toBe(false);
+    done();
   }
 
   getMetaData(1, callback)
@@ -13,11 +13,11 @@ test('getMetaData should pass back an object, but not an array', done => {
 
 test('getMetaData still passes back an object when given a product id that doesn\'t return results', done => {
   let callback = (err, data) => {
-      expect(typeof data).toBe('object');
-      expect(data).toHaveProperty('product_id');
-      expect(err).toBe(null)
-      done();
-    }
+    expect(err).toBe(null)
+    expect(typeof data).toBe('object');
+    expect(data).toHaveProperty('product_id');
+    done();
+  }
 
   getMetaData(987, callback)
 })
